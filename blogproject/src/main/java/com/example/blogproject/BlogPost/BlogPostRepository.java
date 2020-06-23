f44e074e8e3fead46a1b1e8a327eb198ca42e8b9
anupama <anupama_avula@yahoo.com>
2020-06-23 15:12:30 -0400

    Commit

---
+++ blogproject/src/main/java/com/example/blogproject/BlogPost/BlogPostRepository.java
@@ -0,0 +1,9 @@
+
+package com.example.blogproject.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository  extends CrudRepository<BlogPost, Long>
+{
+    
+}
